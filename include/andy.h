94777e46730aab7b48b67fa29f168894d6898e8d
Andy Cooper <andy@coolioh.com>
2024-08-26 18:00:41 -0400

    Updated calibration for filter and pre-amp

--- include/andy.h
+++ include/andy.h
@@ -1,3 +1,9 @@
-float ADC_HIGH = 4.073;
+/*
+   Setting for TM-V71
+     - 1500 Hz
+     - 2.5 kHz
+*/
+float ADC_HIGH = 4.815;
+////float ADC_HIGH = 4.073;
 //float ADC_LOW = 17.8; // 1500Hz
-float ADC_LOW = 17.41;  // 1247Hz
+float ADC_LOW = 5.752;  // 1247Hz
